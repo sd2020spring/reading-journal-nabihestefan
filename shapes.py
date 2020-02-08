77549903bdd805d73ffcd2619d274b83ca414fde
nabihestefan <estefan.nabih@gmail.com>
2020-02-08 14:59:54 -0500

    Completed Reading Journal 5

--- shapes.py
+++ shapes.py
@@ -1,45 +1,95 @@
 import turtle
-
-#------------------------------------------------------------------------------
-# Starter code to get things going
-# (feel free to delete once you've written your own functions
-#------------------------------------------------------------------------------
+import math
 
 # Create the world, and a turtle to put in it
 bob = turtle.Turtle()
 
-# Get moving, turtle!
-bob.fd(100)
 
-# Wait for the user to close the window
-turtle.mainloop()
-
-
-#------------------------------------------------------------------------------
-# Make some shapes
-#   Work through exercises 1-4 in Chapter 4.3.
-#------------------------------------------------------------------------------
-
-# NOTE: for part 2 of 4.3, you will add another parameter to this function
-def square(t):
+#Makes a square with length length
+def square(t, length):
     """
     Draw a square using Turtle t
 
     >>> don = turtle.Turtle()
     >>> square(don)
     """
-    pass # Delete this line and replace it with your code
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+#Makes a polygon with length length and n sides
+def polygon(t, n, length):
+    """
+    Draw a square using Turtle t
+
+    >>> don = turtle.Turtle()
+    >>> square(don)
+    """
+    angle = 360 / n
+    for i in range(int(n)):
+        t.fd(length)
+        t.lt(angle)
 
 
-## Polygon
+#Makes a circle gives radius
+def circle(t, r):
+    circ = 2 * math.pi * r
+    n = int(circ/3) + 3
+    length = circ / n
+    print(n, length)
+    polygon(t, n, length)
 
 
-## Circle
+def pie(t, n, r):
+    angle = 360 / n
+    for i in range(n):
+        triangle(t, r, angle/2)
+        t.lt(angle)
+
+def triangle(t, r, angle):
+    x = r * math.sin(angle * math.pi / 180) * 2
+
+    t.rt(angle)
+    t.fd(r)
+    t.lt(90 + angle)
+    t.fd(x)
+    t.lt(90 + angle)
+    t.fd(r)
+    t.lt(180 - angle)
 
 
-#------------------------------------------------------------------------------
-# Make some art
-#   Complete *at least one of* Exercise 2, 3, 4, or 5 in `shapes.py`.
-#------------------------------------------------------------------------------
+bob.pu()
+bob.bk(400)
+bob.pd()
+square(bob, 50)
 
-# If you come up with some cool drawings you'd like to share with the rest of the class, let us know!
+bob.pu()
+bob.fd(150)
+bob.pd()
+polygon(bob, 6, 100)
+
+bob.pu()
+bob.fd(250)
+bob.pd()
+circle(bob, 70)
+
+bob.pu()
+bob.fd(150)
+bob.lt(90)
+bob.fd(50)
+bob.rt(90)
+bob.pd()
+pie(bob, 5, 50)
+
+bob.pu()
+bob.fd(200)
+bob.lt(90)
+bob.fd(50)
+bob.rt(90)
+bob.pd()
+pie(bob, 10, 100)
+
+
+bob.hideturtle()
+
+turtle.mainloop()
